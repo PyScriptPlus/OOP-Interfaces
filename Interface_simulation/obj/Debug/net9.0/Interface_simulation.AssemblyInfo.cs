21f0394b29f18f35687a2a5f9da9ddd178aa4220
PyScriptPlus <mohamadexbier@gmail.com>
2026-02-08 00:56:22 +0330

    Ziad mohem nist!

--- Interface_simulation/obj/Debug/net9.0/Interface_simulation.AssemblyInfo.cs
+++ Interface_simulation/obj/Debug/net9.0/Interface_simulation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interface_simulation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+560ac915b06ad801c9620656bca03c167bb08f26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a44dd9107e467372fe2f6b9b903e64eab283878a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interface_simulation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interface_simulation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
