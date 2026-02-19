479d0037b458858f4296663474a026f58d08cf3d
PyScriptPlus <mohamadexbier@gmail.com>
2026-02-20 00:21:57 +0330

    Writing code about (Generic for Class) with C#

---
+++ Generic_for_Class/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_for_Class
+{
+
+    // Generic Class daghighan chiye?
+    // Ya'ni classi ke DataType hayi ke dakhel hengham ta'rif class moshakhas 
+    // Nemishavad.
+    // Balke vaghti Instance mikonim meghdareshun moshakhas mishe
+    // Bejaye inke class faghat ba int OR string kar kone betune hame DataType ha
+    // Kar konad.
+
+    // Sakhatar codesh:
+    // Mikham 2 ta noe bargardunam
+    public class MyClass<T,U>
+    {
+        public T? myValue; // Nullable
+        public U? myProp { get; set; }
+    }
+}
---
+++ Generic_for_Class/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Generic_for_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass<string,int> @MyClass = new();
+            MyClass.myProp = 50;
+            Console.WriteLine(MyClass.myValue = "Number is: " + MyClass.myProp);
+
+        }
+    }
+}
